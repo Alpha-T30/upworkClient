2193a6099680ac07f6106373a92407c45cbcf327
Alpha-T30 <enamult300@gmail.com>
2023-12-13 19:18:01 +0600

    folder name changes from server to app

--- seed.js
+++ seed.js
@@ -1,14 +1,14 @@
 // const chalk = require('chalk');
-const { User, Student } = require('./server/db/Models/index');
-const { db } = require('./server/db/db');
-require('dotenv').config();
+const { User, Student } = require("./app/db/Models/index");
+const { db } = require("./app/db/db");
+require("dotenv").config();
 
 const sync = async (force = false) => {
   try {
     await db.sync({ force });
     console.log(`DB successfully connected, and synced. Force: ${force}`);
   } catch (e) {
-    console.log('Error while connecting to database');
+    console.log("Error while connecting to database");
     throw e;
   }
 };
@@ -17,23 +17,23 @@ const seed = async () => {
   await sync(true);
 
   User.create({
-    username: 'kwonadmin',
-    password: 'password1234',
+    username: "kwonadmin",
+    password: "password1234",
     firstName: "Sang-Hyuk",
     lastName: "Kwon",
     userType: "admin",
     isAdmin: true,
-    students: [58810]
+    students: [58810],
   });
 
-  Student.create( {
+  Student.create({
     studentID: 58810,
     firstName: "Andrew",
     lastName: "Kwon",
     grade: 9,
-  })
+  });
 };
 
 seed();
 
-console.log('Data is seeded');
+console.log("Data is seeded");
