466368e0f1fc795310da8baa7433823ff399bdc9
Alpha-T30 <enamult300@gmail.com>
2023-12-13 20:15:02 +0600

    database_url added

--- app/db/db.js
+++ app/db/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const DATABASE_URL = process.env.DATABASE_PRIVATE_URL;
+const DATABASE_URL = process.env.DATABASE_URL;
 //  const DATABASE_URL = process.env.DATABASE_URL || 'postgres://postgres:admin@localhost:5432/DVDsRUs';
 
 const db = new Sequelize(DATABASE_URL, {
